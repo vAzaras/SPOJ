1195bd36008a58dcc2717aebd1760e23e81df352
vAzaras <adam.ludwiczak98@gmail.com>
2020-08-19 03:04:56 +0200

    Test1

---
+++ xxx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xxx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
